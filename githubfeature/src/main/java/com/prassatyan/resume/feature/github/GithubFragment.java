c6054b09e859a7303700263b08eea0ba926b58ee
Satya <satya.kaikala.786@gmail.com>
2018-07-16 13:27:00 -0500

    * Fixed build issues

--- githubfeature/src/main/java/com/prassatyan/resume/feature/github/GithubFragment.java
+++ githubfeature/src/main/java/com/prassatyan/resume/feature/github/GithubFragment.java
@@ -6,9 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.prassatyan.githubfeature.R;
-import com.praszapps.githubfeature.R;
-
 import androidx.fragment.app.Fragment;
 
 
