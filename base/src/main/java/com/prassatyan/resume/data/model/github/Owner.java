16928cb1fc2294df866ef4916499be71789d976f
Prasan Pani <prasannajeet89@gmail.com>
2018-07-22 17:38:12 -0500

    Added Google AutoValue and supporting classes for Parcelable and GSON

--- base/src/main/java/com/prassatyan/resume/data/model/github/Owner.java
+++ base/src/main/java/com/prassatyan/resume/data/model/github/Owner.java
@@ -1,8 +1,10 @@
 package com.prassatyan.resume.data.model.github;
 
-import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.SerializedName;
 
 /**
@@ -10,167 +12,29 @@ import com.google.gson.annotations.SerializedName;
  *
  * @author Satya
  */
-public class Owner implements Parcelable {
+@AutoValue
+public abstract class Owner implements Parcelable {
 
-    @SerializedName("name")
-    private String name; // "name" : "Android"
+    public static Owner create(String name, String email, String location, String profileUrl, String reposUrl, String hireable) {
+        return new AutoValue_Owner(name, email, location, profileUrl, reposUrl, hireable);
+    }
 
-    @SerializedName("email")
-    private String email; // "email" : "Android@google.com"
+    public static TypeAdapter<Owner> typeAdapter(Gson gson) {
+        return new AutoValue_Owner.GsonTypeAdapter(gson);
+    }
 
-    @SerializedName("location")
-    private String location; // "location" : "Plano"
+    abstract String name(); // "name" : "Android"
+
+    abstract String email(); // "email" : "Android@google.com"
+
+    abstract String location(); // "location" : "Plano"
 
     @SerializedName("html_url")
-    private String profileUrl; // "html_url" : "http://github.com/repo"
+    abstract String profileUrl(); // "html_url" : "http://github.com/repo"
 
     @SerializedName("repos_url")
-    private String reposUrl; // "repos_url" : "http://github.com/repo"
+    abstract String reposUrl(); // "repos_url" : "http://github.com/repo"
 
-    @SerializedName("hireable")
-    private String hireable; // "hireable" : null
+    abstract String hireable(); // "hireable" : null
 
-    /**
-     * Get name
-     *
-     * @return
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Set name
-     *
-     * @param name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Get email
-     *
-     * @return
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Set email
-     *
-     * @param email
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * Get location
-     *
-     * @return
-     */
-    public String getLocation() {
-        return location;
-    }
-
-    /**
-     * Set location
-     *
-     * @param location
-     */
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    /**
-     * Get profileUrl
-     *
-     * @return
-     */
-    public String getProfileUrl() {
-        return profileUrl;
-    }
-
-    /**
-     * Set profileUrl
-     *
-     * @param profileUrl
-     */
-    public void setProfileUrl(String profileUrl) {
-        this.profileUrl = profileUrl;
-    }
-
-    /**
-     * Get reposUrl
-     *
-     * @return
-     */
-    public String getReposUrl() {
-        return reposUrl;
-    }
-
-    /**
-     * Set reposUrl
-     *
-     * @param reposUrl
-     */
-    public void setReposUrl(String reposUrl) {
-        this.reposUrl = reposUrl;
-    }
-
-    /**
-     * Get hireable
-     *
-     * @return hireable
-     */
-    public String getHireable() {
-        return hireable;
-    }
-
-    /**
-     * Set hireable
-     *
-     * @param hireable
-     */
-    public void setHireable(String hireable) {
-        this.hireable = hireable;
-    }
-
-    protected Owner(Parcel in) {
-        name = in.readString();
-        email = in.readString();
-        location = in.readString();
-        profileUrl = in.readString();
-        reposUrl = in.readString();
-        hireable = in.readString();
-    }
-
-    public static final Creator<Owner> CREATOR = new Creator<Owner>() {
-        @Override
-        public Owner createFromParcel(Parcel in) {
-            return new Owner(in);
-        }
-
-        @Override
-        public Owner[] newArray(int size) {
-            return new Owner[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(name);
-        parcel.writeString(email);
-        parcel.writeString(location);
-        parcel.writeString(profileUrl);
-        parcel.writeString(reposUrl);
-        parcel.writeString(hireable);
-    }
 }
--- githubfeature/proguard-rules.pro
+++ githubfeature/proguard-rules.pro
@@ -19,3 +19,9 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+# Retain generated classes that end in the suffix
+-keepnames class **_GsonTypeAdapter
+
+# Prevent obfuscation of types which use @GenerateTypeAdapter since the simple name
+# is used to reflectively look up the generated adapter.
+-keepnames @com.ryanharter.auto.value.gson.GenerateTypeAdapter class *
