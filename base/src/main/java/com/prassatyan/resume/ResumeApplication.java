61fc570314d2e43684d18b248431e4df42463dca
Prasan Pani <prasannajeet89@gmail.com>
2018-08-05 23:39:57 -0500

    Lazy dagger injection
    Signed-off-by: Prasan Pani <prasannajeet89@gmail.com>

--- base/src/main/java/com/prassatyan/resume/ResumeApplication.java
+++ base/src/main/java/com/prassatyan/resume/ResumeApplication.java
@@ -5,7 +5,10 @@ import android.app.Application;
 import com.prassatyan.resume.data.local.DataStorage;
 import com.prassatyan.resume.data.model.github.Owner;
 import com.prassatyan.resume.data.model.github.Repo;
+import com.prassatyan.resume.di.DaggerGithubComponent;
 import com.prassatyan.resume.di.GithubComponent;
+import com.prassatyan.resume.di.OwnerModule;
+import com.prassatyan.resume.di.RepoModule;
 
 import javax.inject.Inject;
 
@@ -17,11 +20,8 @@ import javax.inject.Inject;
 public class ResumeApplication extends Application {
 
     GithubComponent component;
-
-
     @Inject
     Owner owner;
-
     @Inject
     Repo repo;
 
@@ -30,5 +30,18 @@ public class ResumeApplication extends Application {
     public void onCreate() {
         super.onCreate();
         DataStorage.initialize(this);
+        component = DaggerGithubComponent.builder().ownerModule(new OwnerModule()).repoModule(new RepoModule()).build();
+    }
+
+    public GithubComponent getComponent() {
+        return component;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public Repo getRepo() {
+        return repo;
     }
 }
