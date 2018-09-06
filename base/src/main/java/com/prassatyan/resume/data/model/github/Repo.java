d29f0187d3453f82127dc5d6026351f47977f065
Satya <satya.kaikala.786@gmail.com>
2018-09-05 22:41:43 -0500

    * Replaced Gson with Moshi * Removed un-used dependencies in gradle

--- base/src/main/java/com/prassatyan/resume/data/model/github/Repo.java
+++ base/src/main/java/com/prassatyan/resume/data/model/github/Repo.java
@@ -3,9 +3,9 @@ package com.prassatyan.resume.data.model.github;
 import android.os.Parcelable;
 
 import com.google.auto.value.AutoValue;
-import com.google.gson.Gson;
-import com.google.gson.TypeAdapter;
-import com.google.gson.annotations.SerializedName;
+import com.squareup.moshi.Json;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 
 
 /**
@@ -16,35 +16,35 @@ import com.google.gson.annotations.SerializedName;
 @AutoValue
 public abstract class Repo implements Parcelable {
 
+    @Json(name = "id")
+    abstract long repoId(); //"id": 99999
+
+    @Json(name = "name")
+    abstract String repoName(); //"name": "Hello-World"
+
+    @Json(name = "full_name")
+    abstract String repoFullName(); //"full_name":"octocat/Hello-World"
+
+    @Json(name = "html_url")
+    abstract String repoUrl(); // "html_url":"https://github.com/octocat/Hello-World"
+
+    @Json(name = "description")
+    abstract String repoDescription(); //  "description":"This your first repo!"
+
+    @Json(name = "fork")
+    abstract boolean isForked(); //  "fork":true
+
+    @Json(name = "created_at")
+    abstract String repoCreatedAt(); // "created_at":"2011-01-26T19:01:12Z"
+
+    @Json(name = "updated_at")
+    abstract String repoLastUpdatedAt(); //  "updated_at":"2011-01-26T19:14:43Z"
+
     public static Repo create(long repoId, String repoName, String repoFullName, String repoUrl, String repoDescription, boolean isForked, String repoCreatedAt, String repoUpdatedAt) {
         return new AutoValue_Repo(repoId, repoName, repoFullName, repoUrl, repoDescription, isForked, repoCreatedAt, repoUpdatedAt);
     }
 
-    public static TypeAdapter<Repo> typeAdapter(Gson gson) {
-        return new AutoValue_Repo.GsonTypeAdapter(gson);
+    public static JsonAdapter<Repo> repoJsonAdapter(Moshi moshi) {
+        return new AutoValue_Repo.MoshiJsonAdapter(moshi);
     }
-
-    @SerializedName("id")
-    abstract long repoId(); //"id": 99999
-
-    @SerializedName("name")
-    abstract String repoName(); //"name": "Hello-World"
-
-    @SerializedName("full_name")
-    abstract String repoFullName(); //"full_name":"octocat/Hello-World"
-
-    @SerializedName("html_url")
-    abstract String repoUrl(); // "html_url":"https://github.com/octocat/Hello-World"
-
-    @SerializedName("description")
-    abstract String repoDescription(); //  "description":"This your first repo!"
-
-    @SerializedName("fork")
-    abstract boolean isForked(); //  "fork":true
-
-    @SerializedName("created_at")
-    abstract String repoCreatedAt(); // "created_at":"2011-01-26T19:01:12Z"
-
-    @SerializedName("updated_at")
-    abstract String repoLastUpdatedAt(); //  "updated_at":"2011-01-26T19:14:43Z"
 }
