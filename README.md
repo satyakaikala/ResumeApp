a502412c40359f373eb20c21b431036f33396af2
Prasan Pani <prasannajeet89@gmail.com>
2018-09-13 21:52:48 -0500

    Added Google's Open Source Notices library

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 [![CircleCI](https://circleci.com/gh/PraSatyan/ResumeApp/tree/develop.svg?style=svg)](https://circleci.com/gh/PraSatyan/ResumeApp/tree/develop) [![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2FPraSatyan%2FResumeApp.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2FPraSatyan%2FResumeApp?ref=badge_shield)
-## Welcome to GitHub Pages
+## Resume Application - Resume, Github and LinkedIn in one Android app
 
-You can use the [editor on GitHub](https://github.com/PraSatyan/ResumeApp/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
+This application has been developed by [Satya Chaitanya Kaikala](https://github.com/satyakaikala) and [Prasannajeet Pani](https://github.com/prasannajeet/) to showcase our resume, Github and LinkedIn profiles to anyone who is interested to look at our professional profiles in form of an Android app.
 
-Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
+This application uses Product Flavours from Android gradle plugin to work separately for both our profiles
 
 ### Markdown
 
 Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
+
+We will update the rest of the ReadMe as development proceeds.
 
 ```markdown
 Syntax highlighted code block
--- resumefeature/src/main/java/com/prassatyan/resume/feature/resumemain/ui/activity/MainActivity.java
+++ resumefeature/src/main/java/com/prassatyan/resume/feature/resumemain/ui/activity/MainActivity.java
@@ -1,16 +1,21 @@
 package com.prassatyan.resume.feature.resumemain.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import com.prassatyan.resume.feature.resumemain.R;
 import com.prassatyan.resume.feature.resumemain.ui.github.fragment.GithubFragment;
 import com.prassatyan.resume.feature.resumemain.ui.linkedin.LinkedinFragment;
 import com.prassatyan.resume.feature.resumemain.ui.resume.ResumeFragment;
+import com.prassatyan.resume.ui.ResumeBaseActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ResumeBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_placeholder, new ResumeFragment()).commit();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
         // TODO implement data binding once fragment switching works
@@ -32,5 +40,21 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_license) {
+
+            startActivity(new Intent(this, OssLicensesMenuActivity.class));
+
+        }
+        return false;
     }
 }
